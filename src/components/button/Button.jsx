f20b27659ace6709744cb7a2f343f648cdf39559
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2021-01-27 17:08:25 +0100

    added 'as' prop to Button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 import style from './button.module.scss'
 import Shadow from '../../assets/svg/Shadow.svg'
 
-export default function Button({ children, className, ...rest }) {
-
-
+export default function Button({ children, className, as = 'link', ...rest }) {
     return (
         <div className={style.button__container + ' ' + className}>
-            <Link className={style.button} {...rest}>
-                {children}
-            </Link>
+            {
+                as === 'button' ?
+                    (
+                        <button className={style.button} {...rest}>
+                            {children}
+                        </button>
+                    ) : (
+                        <Link className={style.button} {...rest}>
+                            {children}
+                        </Link>
+                    )
+            }
             <Shadow className={style.button__shadow} />
-        </div>
+        </div >
 
     )
+}
+
+Button.propTypes = {
+    as: PropTypes.oneOf(['link', 'button'])
 }
