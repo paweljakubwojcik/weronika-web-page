cff7ea74bffd26fe24fe8dc5a2797295fc0a6a5f
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2021-02-07 16:55:34 +0100

    added label to phone number

--- src/components/contact-section/PhoneNumber.jsx
+++ src/components/contact-section/PhoneNumber.jsx
@@ -6,17 +6,17 @@ export default function PhoneNumber({ timeout }) {
     const number = '999243111'
 
     return (
-
-
-        <a href={`tel:${number}`} className='phone-number'>
-            {number.split('').map((digit, i) =>
-                <span key={i} style={{
-                    transitionDelay: `${i * timeout / 12}ms`,
-                    transitionDuration: `${timeout / 3}ms`
-                }}>
-                    {digit}
-                </span>)}
-        </a>
-
+        <div className='phone-number__container'>
+            <label>Click to call</label>
+            <a href={`tel:${number}`} className='phone-number'>
+                {number.split('').map((digit, i) =>
+                    <span key={i} style={{
+                        transitionDelay: `${i * timeout / 12}ms`,
+                        transitionDuration: `${timeout / 3}ms`
+                    }}>
+                        {digit}
+                    </span>)}
+            </a>
+        </div>
     )
 }
